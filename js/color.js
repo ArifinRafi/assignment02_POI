7de508b7f2a936f7204bb4b3610879a47bdf4544
arifin rafi <ahmedrafi364@gmail.com>
2023-02-21 14:35:21 +0600

    js added with button and color

---
+++ js/color.js
@@ -0,0 +1,24 @@
+document.getElementById('red').addEventListener('click', function()
+{
+    document.getElementById('box').style.background="red";
+}
+)
+
+document.getElementById('yellow').addEventListener('click', function()
+{
+
+    document.getElementById('box').style.background="yellow";
+
+}
+
+ )
+
+document.getElementById('green').addEventListener('click', function() 
+{
+    document.getElementById('box').style.background="green";
+})
+
+
+document.getElementById("blue").addEventListener('click', function() {
+    document.getElementById('box').style.background='blue';
+})
