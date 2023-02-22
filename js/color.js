c07cc7e2b97106761633e2977066f16ed64adb12
arifin rafi <ahmedrafi364@gmail.com>
2023-02-22 16:47:39 +0600

    quote generator added for all the buttons

--- js/color.js
+++ js/color.js
@@ -2,7 +2,7 @@
 let quotes = ["When you're suffering you are on the road to succes",
 "We all die Alone", "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela",
 "The way to get started is to quit talking and begin doing. -Walt Disney", "“So remember Me; I will remember you. And be grateful to Me and do not deny Me.” Surah Baqarah Verse 152"
-, "Indeed we belong to Allah, and indeed to Him we will return” Ayat 156"
+,"Indeed we belong to Allah, and indeed to Him we will return” Ayat 156"
 ];
 
 
@@ -24,11 +24,13 @@ document.getElementById('red').addEventListener('click', function()
 
 document.getElementById('yellow').addEventListener('click', function()
 {
-
-    document.getElementById('box').style.background='yellow';
+    document.getElementById('box').style.background="yellow";
+    
     let random = Math.floor(Math.random() * quotes.length);
     console.log(quotes[random]);
     field1.innerHTML = quotes[random];
+
+    
 
 }
 
@@ -37,21 +39,23 @@ document.getElementById('yellow').addEventListener('click', function()
 document.getElementById('green').addEventListener('click', function() 
 {
     document.getElementById('box').style.background="green";
+    
     let random = Math.floor(Math.random() * quotes.length);
     console.log(quotes[random]);
     field1.innerHTML = quotes[random];
+    
 })
 
 
 document.getElementById("blue").addEventListener('click', function() {
-    document.getElementById('box').style.background='blue';
+    document.getElementById('box').style.background="blue";
+    
     let random = Math.floor(Math.random() * quotes.length);
     console.log(quotes[random]);
     field1.innerHTML = quotes[random];
+    
 })
 
-
-// Quotes Generator
 
 
 
